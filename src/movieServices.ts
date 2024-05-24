d384b3c861576be251ded1c67b008fa875f5cf1f
Fatima Assi <Fatima.assi@medieinstitutet.se>
2024-05-24 15:59:16 +0200

    edit commit

--- src/movieServices.ts
+++ src/movieServices.ts
@@ -8,11 +8,11 @@ export const getMovies = async ():Promise<IMovie[]> => {
     try{
     const response = await axios.get<IMovie[]>("...");
     return response.data;
-    //return {success: true, movie: response.data, error:""};
+    
     }
     catch(error){
         console.log(error);
         return [];
-        //return{ success: false, movie: [], error:JSON.stringify(error)};
+        
     }
 };
